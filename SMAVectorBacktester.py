405511ea3927cae3d1362e91f66708af619ab474
ioancw <ioan.williams@gmail.com>
2017-01-27 23:16:37 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	PackingSantasSack.py

---
+++ SMAVectorBacktester.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 21:22:37 2017
+
+@author: ioanwilliams
+"""
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as web
+from scipy.optimize import brute
+
+
+class SMAVectorBacktester(object):
+    ''' Class for the vectorized backtesting of SMA-based trading strategies.
+
+    Attributes
+    ==========
+    symbol: str
+        Yahoo! Finance symbol with which to work with
+    SMA1: int
+        time window in days for shorter SMA
+    SMA2: int
+        time window in days for longer SMA
+    start: str
+        start date for data retrieval
+    end: str
+        end date for data retrieval
+
+    Methods
+    =======
+    get_data:
+        retrieves and prepares the base data set
+    set_parameters:
+        sets one or two new SMA parameters
+    run_strategy:
+        runs the backtest for the SMA-based strategy
+    plot_results:
+        plots the performance of the strategy compared to the symbol
+    update_and_run:
+        updates SMA parameters and returns the (negative) absolute performance
+    optimize_parameters:
+        implements a brute force optimizeation for the two SMA parameters
+    '''
+
+    def __init__(self, symbol, SMA1, SMA2, start, end):
+        self.symbol = symbol
+        self.SMA1 = SMA1
+        self.SMA2 = SMA2
+        self.start = start
+        self.end = end
+        self.results = None
+        self.get_data()
+
+    def get_data(self):
+        raw = web.DataReader(self.symbol, data_source='yahoo',
+                             start=self.start, end=self.end)['Adj Close']
+        raw = pd.DataFrame(raw)
+        raw.rename(columns={'Adj Close': 'price'}, inplace=True)
+        raw['return'] = np.log(raw / raw.shift(1))
+        raw['SMA1'] = raw['price'].rolling(self.SMA1).mean()
+        raw['SMA2'] = raw['price'].rolling(self.SMA2).mean()
+        self.data = raw
+
+    def set_parameters(self, SMA1=None, SMA2=None):
+        if SMA1 is not None:
+            self.SMA1 = SMA1
+            self.data['SMA1'] = self.data['price'].rolling(self.SMA1).mean()
+        if SMA2 is not None:
+            self.SMA2 = SMA2
+            self.data['SMA2'] = self.data['price'].rolling(self.SMA2).mean()
+
+    def run_strategy(self):
+        data = self.data.copy().dropna()
+        data['position'] = np.where(data['SMA1'] > data['SMA2'], 1, -1)
+        data['strategy'] = data['position'].shift(1) * data['return']
+        data['creturns'] = data['return'].cumsum().apply(np.exp)
+        data['cstrategy'] = data['strategy'].cumsum().apply(np.exp)
+        self.results = data
+        # absolute performance of the strategy
+        aperf = data['cstrategy'].ix[-1]
+        # out-/underperformance of strategy
+        operf = aperf - data['creturns'].ix[-1]
+        return round(aperf, 2), round(operf, 2)
+
+    def plot_results(self):
+        if self.results is None:
+            print('No results to plot yet. Run a strategy.')
+        title = '%s | SMA1 = %d, SMA2 = %d' % (self.symbol, self.SMA1, self.SMA2)
+        self.results[['creturns', 'cstrategy']].plot(title=title, figsize=(10, 6))
+
+    def update_and_run(self, SMA):
+        self.set_parameters(int(SMA[0]), int(SMA[1]))
+        return -self.run_strategy()[0]
+
+    def optimize_parameters(self, SMA1_range, SMA2_range):
+        opt = brute(self.update_and_run, (SMA1_range, SMA2_range), finish=None)
+        return opt, -self.update_and_run(opt)
+
+
+if __name__ == '__main__':
+    smabt = SMAVectorBacktester('DIS', 42, 252, '2009-12-31', '2016-12-31')
+    print(smabt.run_strategy())
+    smabt.set_parameters(SMA1=20, SMA2=100)
+    print(smabt.run_strategy())
+    smabt.plot_results()
+    # print(smabt.optimize_parameters((1, 56, 2), (150, 300, 4)))
