75151878e76d7513ed6ab8d023fc5aa0f880ddda
ioancw <ioan.williams@gmail.com>
2017-01-27 23:10:20 +0000

    Reformatting

--- PackingSantasSack.py
+++ PackingSantasSack.py
@@ -13,9 +13,9 @@ class Sack(object):
     def __str__(self):
         return 'Sack(volume=%d, boxes=%s)' % (self.sum, self.boxes)
 
-        
+
 def pack_sleigh(boxes, volume, sort_boxes, sort_sacks):
-    #sort boxes so that biggest boxes are packed first.
+    # sort boxes so that biggest boxes are packed first.
     if sort_boxes:
         boxes = sorted(boxes, reverse=True)
     sleigh = []
@@ -29,13 +29,13 @@ def pack_sleigh(boxes, volume, sort_boxes, sort_sacks):
             sack = Sack()
             sack.add_box(box)
             sleigh.append(sack)
-            #sort sacks so largest sacks get filled first.
+            # sort sacks so largest sacks get filled first.
             if sort_sacks:
                 sleigh.sort(key=sack_volume, reverse=True)
 
     return sleigh
 
-    
+
 def sack_volume(sack):
     return sack.sum
 
@@ -43,13 +43,15 @@ def sack_volume(sack):
 if __name__ == '__main__':
     import random
 
+
     def pack(boxes, volume, sort, sort_sacks):
         sleigh = pack_sleigh(boxes, volume, sort, sort_sacks)
 
         print(len(boxes), ' boxes packed into ', len(sleigh), ' sacks:')
-        #for sack in sleigh.sacks:
-            #print(sack)
+        # for sack in sleigh.sacks:
+        # print(sack)
         return len(sleigh)
+
 
     print('--')
     method1 = []
@@ -64,6 +66,6 @@ if __name__ == '__main__':
         print('--')
 
     print('Simulated ', simulation)
-    print('Method1 boxes:', sum(method1)/len(method1))
-    print('Method2 boxes:', sum(method2)/len(method2))
-    print('Method2 boxes:', sum(method3)/len(method3))
+    print('Method1 boxes:', sum(method1) / len(method1))
+    print('Method2 boxes:', sum(method2) / len(method2))
+    print('Method2 boxes:', sum(method3) / len(method3))
